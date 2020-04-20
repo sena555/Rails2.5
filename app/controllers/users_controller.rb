ce9d5bc9fe3beec3a5d2214bd033b6a21e81b95f
sena555 <sayaka095@gmaii.com>
2020-04-21 00:35:06 +0900

    7.削除機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,11 @@ class UsersController < ApplicationController
    user.update(user_params)
  end
 
+ def destroy
+   user = User.find(params[:id])
+   user.destroy
+ end 
+
  private
  def user_params
     params.require(:user).permit(:name, :age)
